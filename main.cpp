a2ce8711080cd53664e78eba908d57274d2399ad
MisterE1234 <MisterE1234@outlook.com>
2025-10-26 15:33:05 -0700

    2nd Commit

--- main.cpp
+++ main.cpp
@@ -3,11 +3,32 @@
 //This Program is to time different methods of storing information.
 
 #include <iostream>
+#include <vector>
+#include <list>
+#include <set>
 #include <chrono>
+#include <fstream>
 using namespace std;
+using namespace std::chrono;
+
+int SZ_CODES = 20000;
+
 
 int main() {
+
+    //Declaring the different competetors in the race:
+    vector<string> strCode;
+    list<string> listCode;
+    set<string> setCode;
     
+    //Opening the codes for the race:
+    ifstream fin("codes.txt");
+    
+    
+
+
+
+    fin.close();
 
     return 0;
 }
